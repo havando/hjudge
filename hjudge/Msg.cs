a8bf868b3a3ee65c66694c286cb2dc615013c036
贺恩泽 <hez2010@outlook.com>
2017-06-08 23:58:00 +0800

    添加项目文件。

---
+++ hjudge/Msg.cs
@@ -0,0 +1,11 @@
+﻿namespace hjudge
+{
+    public class Msg
+    {
+        public int Type;
+        public byte[] Content;
+        public string Encoding;
+        public int From;
+        public int To;
+    }
+}
