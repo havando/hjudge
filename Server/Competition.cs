f628a7833937cdfcd562d50b90596441924fcba2
贺恩泽 <hez2010@outlook.com>
2017-11-20 20:10:34 +0800

    Finish Cometition Class

---
+++ Server/Competition.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace Server
+{
+    class Competition : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int CompetitionId { get; set; }
+        public string CompetitionName
+        {
+            get => _competitionName;
+            set
+            {
+                _competitionName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CompetitionName"));
+            }
+        }
+
+        public string StartTime
+        {
+            get => _startTime;
+            set
+            {
+                _startTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("StartTime"));
+            }
+        }
+
+        public string EndTime
+        {
+            get => _endTime;
+            set
+            {
+                _endTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EndTime"));
+            }
+        }
+
+        public string[] ProblemSet
+        {
+            get => _problemSet;
+            set
+            {
+                _problemSet = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProblemCount"));
+            }
+        }
+        public int Option { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+        public int ProblemCount => ProblemSet?.Length ?? 0;
+
+        private string _competitionName;
+        private string[] _problemSet { get; set; }
+        private string _startTime;
+        private string _endTime;
+
+    }
+}
